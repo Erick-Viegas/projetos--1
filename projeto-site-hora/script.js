df99f309ecc5f36e5fcf277bea873382f23a28c1
Erick-Viegas <viegas.erick2005@gmail.com>
2024-04-08 18:45:44 -0300

    att

--- projeto-site-hora/script.js
+++ projeto-site-hora/script.js
@@ -7,6 +7,8 @@ var img = window.document.getElementById('imagem')
  var hora = data.getHours()
  var minuto = data.getMinutes()
  var segundo = data.getSeconds()
+ setTimeout(carregar, 1000)
+ 
 
 
 msg.innerHTML = `Agora são ${hora} Horas ${minuto} minutos e ${segundo} segundos no horário de Brasília`
